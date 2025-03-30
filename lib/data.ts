b90567d62135193c032853103f1c4975e00b0c6d
Anikesh Roy <anikeshroy62040@gmail.com>
2025-03-30 16:17:18 +0530

    Fixed Error

--- lib/data.ts
+++ lib/data.ts
@@ -963,31 +963,6 @@ const semestersData: Record<number, SemesterDataType> = {
           notes: [],
         },
       },
-      {
-        id: "cs801",
-        title: "Anikesh Project",
-        code: "CS801",
-        description: "Full-semester practical project implementation",
-        resources: {
-          questions: [],
-          videos: [],
-          videoSolutions: [],
-          notes: [],
-        },
-      },
-
-      {
-        id: "cs801",
-        title: "Anikesh Project 2",
-        code: "CS801",
-        description: "Full-semester practical project implementation",
-        resources: {
-          questions: [],
-          videos: [],
-          videoSolutions: [],
-          notes: [],
-        },
-      },
     ],
   },
 };
