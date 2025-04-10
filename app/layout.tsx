847c068c6a9b242ac103660cc9ca58457df2cddf
Anikesh Roy <anikeshroy62040@gmail.com>
2025-04-10 15:03:44 +0530

    Clarity Integrsted

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,6 +89,15 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1498677281930673"
           crossOrigin="anonymous"
         />
+        <script type="text/javascript">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "r1wau81u49");
+          `}
+        </script>
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
